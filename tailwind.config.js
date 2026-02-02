95b3424c095a5a0b01a18b0e85c040a8d0310d57
treofc-debug <treofc@gmail.com>
2026-02-02 12:23:28 -0300

    v2.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
     content: [
         "./index.html",
-        "./**/*.{js,ts,jsx,tsx}",
+        "./App.tsx",
+        "./index.tsx",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./services/**/*.{js,ts,jsx,tsx}",
+        "./utils/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
